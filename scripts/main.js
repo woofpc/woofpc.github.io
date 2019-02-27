1bf7f1f830814d0653521dc57f9ae61590f5584d
woofpc <47554725+woofpc@users.noreply.github.com>
2019-02-27 15:51:25 +0900

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/mochi.gif') {
+      myImage.setAttribute ('src','images/mochi.jpg');
+    } else {
+      myImage.setAttribute ('src','images/mochi.gif');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName()
+{
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
